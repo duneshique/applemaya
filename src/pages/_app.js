f0564adfe9785c0d44641ce903b19528f698c324
duneshique <june@plus-ex.com>
2026-02-08 13:12:57 +0900

    작업 내용 요약 (예: 마우스 애니메이션 제거)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,6 @@ import { appWithTranslation } from 'next-i18next';
 import "@/styles/globals.css";
 import { ParallaxProvider } from 'react-scroll-parallax';
 import LoadingScreen from '@/components/UI/LoadingScreen';
-import CustomCursor from '@/components/UI/CustomCursor';
 
 import Header from '@/components/Layout/Header';
 import Footer from '@/components/Layout/Footer';
@@ -11,7 +10,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <ParallaxProvider>
       <LoadingScreen />
-      <CustomCursor />
       <div className="flex flex-col min-h-screen">
         <Header />
         <Component {...pageProps} />
